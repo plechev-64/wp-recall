a8f443238896bec6e7b8b1adcb603cbc8d993478
Андрей <@192.168.1.38>
2015-03-10 11:19:10 +0300

    добавлен выбор года при указании даты в поле типа date, указан минимальный и максимальный год для выбора

--- js/datepicker/datepicker-init.js
+++ js/datepicker/datepicker-init.js
@@ -3,6 +3,8 @@ jQuery(function(){
     jQuery(".datepicker").datepicker({
         monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
         dayNamesMin: [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" ],
-        dateFormat: 'dd.mm.yy'
+        dateFormat: 'dd.mm.yy',
+        yearRange: "1950:c+3",
+        changeYear: true
       });
 });
