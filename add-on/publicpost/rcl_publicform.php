d4f04a7cd74af0ba437e2f766c092e46ec9e2dc4
Андрей <@192.168.1.38>
2015-04-17 18:50:41 +0300

    создание шаблона формы публикации и редактирования

--- add-on/publicpost/rcl_publicform.php
+++ add-on/publicpost/rcl_publicform.php
@@ -4,14 +4,14 @@ class Rcl_PublicForm {
     
     public $post_id;//идентификатор записи
     public $post_type; //тип записи
-    public $cats; //список категорий доступных для выбора
+    public $terms; //список категорий доступных для выбора
     public $form_id; //идентификатор формы
 	public $id_upload;
 	public $accept;
 	public $type_editor;
     
     function __construct($atts){
-        global $postdata,$group_id,$rcl_options,$taxs,$user_ID,$wpdb;
+        global $editpost,$group_id,$rcl_options,$user_ID,$formData;
         
         extract(shortcode_atts(array(
             'cats' => false,
@@ -25,21 +25,18 @@ class Rcl_PublicForm {
 		
         $this->post_type = $post_type;
         $this->id_upload = $id_upload;
-        $this->cats = $cats;
+        $this->terms = $cats;
         $this->form_id = $id;
 	$this->accept = 'image/*';
 		
         if(!isset($type_editor)) $this->type_editor = $rcl_options['type_text_editor'];
         else $this->type_editor = $type_editor;
-        
-        $taxs = array();       
-        $taxs = apply_filters('taxonomy_public_form_rcl',$taxs);
-        
+
         if(isset($_GET['rcl-post-edit'])){
             
             $this->post_id = $_GET['rcl-post-edit']; 
-            $postdata = get_post($this->post_id);
-            $this->post_type = $postdata->post_type;
+            $editpost = get_post($this->post_id);
+            $this->post_type = $editpost->post_type;
             
             if($this->post_type=='post-group'){
                 
@@ -55,6 +52,20 @@ class Rcl_PublicForm {
             //if($rcl_options['accept-'.$this->post_type]) $this->accept = $rcl_options['accept-'.$this->post_type];
             if($this->post_type=='task') $this->id_upload = 'freelanceupload';
         }
+        
+        $taxs = array();       
+        $taxs = apply_filters('taxonomy_public_form_rcl',$taxs);
+        
+        $formData = (object)array(
+            'form_id' =>$this->form_id,
+            'post_id' =>$this->post_id,
+            'post_type' =>$this->post_type,
+            'id_upload' =>$this->id_upload,
+            'terms' =>$this->terms,
+            'accept' =>$this->accept,
+            'type_editor' =>$this->type_editor,
+            'taxonomy' =>$taxs
+        );
         
         fileapi_footer_scripts();
         add_filter('public_form_rcl',array(&$this,'add_tags_input'),10);
@@ -75,115 +86,6 @@ class Rcl_PublicForm {
         return $taglist;
     }
     
-    function get_catlist(){
-        
-        global $taxs;
-
-        if(!$taxs[$this->post_type]) return false;
-        
-        if($this->post_type=='post'){ 
-            $cat_list = get_the_category($this->post_id);
-        }else{
-            $post_cat = get_the_terms( $this->post_id, $taxs[$this->post_type] );
-            
-            $Child_Terms = new Get_Child_Terms_Rcl();
-            $cat_list = $Child_Terms->get_terms_post($post_cat);           
-        }
-        return $cat_list;
-    }
-    
-    function get_editor(){
-        global $rcl_options,$postdata;
-        
-        if($rcl_options['media_downloader_recall']==1) $media_buttons = 1;
-        else $media_buttons = 0;
-
-        $tinymce = 0;
-        $quicktags = 0;
-        
-        if($this->type_editor==1||$this->type_editor==3) $tinymce = 1;
-        if($this->type_editor==2||$this->type_editor==3) $quicktags = 1;
-
-        $args = array( 'wpautop' => 1  
-            ,'media_buttons' => $media_buttons  
-            ,'textarea_name' => 'post_content'
-            ,'textarea_rows' => 20  
-            ,'tabindex' => null  
-            ,'editor_css' => ''  
-            ,'editor_class' => 'autosave'  
-            ,'teeny' => 0  
-            ,'dfw' => 0  
-            ,'tinymce' => $tinymce  
-            ,'quicktags' => $quicktags  
-        );
-        
-        $label = apply_filters('label_editor_public_form','Текст публикации:',$this);
-        $editor = '<label>'.$label.'</label>'
-           .$this->get_media_button();
-        
-        ob_start();
-        
-        if(isset($postdata->post_content)) $content = $postdata->post_content;
-        else $content = '';
-        
-        wp_editor( $content, 'contentarea', $args );
-        
-        $editor .= ob_get_contents();
-        ob_end_clean();
-
-        return $editor;
-    }
-    
-    function get_upload_form(){
-        ob_start();
-        new Rcl_Thumb_Form($this->post_id,$this->id_upload);
-        $form = ob_get_contents();
-        ob_end_clean();
-        return $form;
-    }
-    
-    function get_allterms(){
-        
-        global $taxs;
-        
-        if($this->post_type&&!isset($taxs[$this->post_type])) return false;
-        
-        if($this->post_type=='post'||!$this->post_type){
-
-            $catargs = array(   
-                'orderby'   => 'name'  
-                ,'order'    => 'ASC'  
-                ,'hide_empty'   => 0   
-                ,'hierarchical' =>true
-            );
-
-            $allcats = get_categories( $catargs );
-            
-        }else{
-            
-            $term_args = array(  
-                'number'        => 0  
-                ,'offset'       => 0  
-                ,'orderby'      => 'id'  
-                ,'order'        => 'ASC'  
-                ,'hide_empty'   => false  
-                ,'fields'       => 'all'  
-                ,'slug'         => ''  
-                ,'hierarchical' => true  
-                ,'name__like'   => ''  
-                ,'pad_counts'   => false  
-                ,'get'          => ''  
-                ,'child_of'     => 0  
-                ,'parent'       => ''  
-            );  
-            
-            $allcats = get_terms($taxs[$this->post_type], $term_args); 
-            
-        }
-
-        return $allcats;
-    }
-    
     function user_can(){
         global $rcl_options,$user_ID;
         
@@ -200,60 +102,6 @@ class Rcl_PublicForm {
         else return false;
     }
     
-    function get_custom_fields($post_id){
-        return get_custom_fields_list_rcl($post_id,$this->post_type,$this->form_id);
-    }
-    
-    function get_termlist(){
-        global $group_id,$rcl_options,$wpdb,$taxs,$options_gr;
-        
-        if(!isset($taxs[$this->post_type])&&$this->post_id) return false;
-        
-        $cnt = 0;
-        $ctg = 0;
-        
-        if($this->cats) $ctg = $this->cats;
-        
-        if($this->post_type=='post'){
-            $cf = get_custom_fields_rcl($this->post_id,$this->post_type,$this->form_id);
-            
-            if(!$ctg) if(isset($cf['options']['terms'])) $ctg = $cf['options']['terms'];
-            
-            if(!$ctg) $ctg = $rcl_options['id_parent_category'];
-
-           /*if($ctg){
-               $ctg_ar = explode(',',$ctg);
-               $cnt_c = count($ctg_ar);
-           }*/
-
-           $cnt = (isset($rcl_options['count_category_post']))? $rcl_options['count_category_post']:0;
-        }
-        
-        if($this->post_type=='post-group'){
-            $options_gr = get_options_group($group_id);
-            $catlist = get_tags_list_group_rcl($options_gr['tags'],$this->post_id);
-
-        }else{
-            $cat_list = '';
-            if(!$cnt) $cnt = 1;
-            $allcats = $this->get_allterms();
-            if($this->post_id) $cat_list = $this->get_catlist();
-            $sel = new List_Terms_rcl();
-            
-            $catlist = $sel->get_select_list($allcats,$cat_list,$cnt,$ctg);
-            
-        }
-        //print_r($ctg);
-        if(!$catlist) return false; 
-        return '<label>Категория:</label>'.$catlist;
-    }
-    
-    function get_media_button(){
-        global $rcl_options;
-        if($rcl_options['media_downloader_recall']!=1)
-            return get_button_rcl('Добавить медиафайл','#',array('icon'=>'fa-folder-open','id'=>'get-media-rcl'));
-    }
-
     function add_tags_input($fls){
         
         if($this->post_type=='post') 
@@ -322,19 +170,8 @@ class Rcl_PublicForm {
         return $cnt;
     }
     
-    function get_title(){
-        global $postdata;
-        if(isset($postdata->post_title)) $title = $postdata->post_title;
-        else $title = '';
-        $label = apply_filters('label_title_public_form','Заголовок',$this);
-        return '<label>
-            '.$label.' <span class="required">*</span>:
-            </label>
-            <input type="text" maxlength="150" required value="'.$title.'" name="post_title" id="post_title_input">';
-    }
-    
     function public_form(){
-        global $user_ID;
+        global $user_ID,$formFields;
 
             if(!$user_ID) return '<p align="center">Вы должны быть авторизованы для возможности делать публикации.<br>Войдите или зарегистрируйтесь</p>';
             
@@ -355,9 +192,7 @@ class Rcl_PublicForm {
             	'upload'=>true
             );
             
-            $formfields = apply_filters('fields_public_form_rcl',$formfields,$this);
-				
-            $public_fields = ($formfields['custom_fields'])? $this->get_custom_fields($this->post_id): false;
+            $formFields = apply_filters('fields_public_form_rcl',$formfields,$this);
 
             $form = '<div class="public_block">';
                 
@@ -381,24 +216,14 @@ class Rcl_PublicForm {
                 $form .= ($this->post_id)? 'edit-form' : 'public-form';
                 $form .= '" onsubmit="document.getElementById(\'edit-post-rcl\').disabled=true;document.getElementById(\'edit-post-rcl\').value=\'Идет отправка, пожалуйста, подождите..\';"  action="" method="post" enctype="multipart/form-data">
                 '.wp_nonce_field('edit-post-rcl','_wpnonce',true,false);
-
-                    if($formfields['title']) $form .= $this->get_title();
-
-                    if($formfields['termlist']) $form .= $this->get_termlist();
-
-                    if($formfields['editor']) $form .= $this->get_editor();
-
-                    if($formfields['upload']) $form .= $this->get_upload_form();
                     
-                    $fields = '';
-
-                    $form .= apply_filters('public_form_rcl',$fields,$this);
-
-                   if($public_fields) $form .= $public_fields;
+                    if(get_template_rcl($this->post_type.'-form.php',__FILE__)) $form .= get_include_template_rcl($this->post_type.'-form.php',__FILE__);
+                        else $form .= get_include_template_rcl('public-form.php',__FILE__);
 
                     $form .= $this->submit_and_hidden()
 
                . '</form>';
+                    
                $after = '';
                $form .= apply_filters('after_public_form_rcl',$after,$this);
                
@@ -406,4 +231,142 @@ class Rcl_PublicForm {
 
         return $form;
     }
+}
+
+function the_public_title(){
+    global $editpost;
+    $title = (isset($editpost->post_title))? $editpost->post_title: false;
+    echo $title;
+}
+
+function the_public_termlist($tax=false){
+    global $group_id,$rcl_options,$options_gr,$formData;
+    if($tax) $formData->taxonomy[$formData->post_type] = $tax;  
+    if(!isset($formData->taxonomy[$formData->post_type])&&$formData->post_id) return false;
+
+    $ctg = ($formData->terms)? $formData->terms: 0;
+
+    if($formData->post_type=='post'){
+        $cf = get_custom_fields_rcl($formData->post_id,$formData->post_type,$formData->form_id);
+        if(!$ctg) $ctg = (isset($cf['options']['terms']))? $cf['options']['terms']: $ctg = $rcl_options['id_parent_category'];
+        $cnt = (isset($rcl_options['count_category_post']))? $rcl_options['count_category_post']:0;
+    }
+
+    if($formData->post_type=='post-group'){
+        $options_gr = get_options_group($group_id);
+        $catlist = get_tags_list_group_rcl($options_gr['tags'],$formData->post_id);
+
+    }else{
+        $cnt = (!$cnt)? 1: $cnt;
+        $cat_list = ($formData->post_id)? get_public_catlist(): '';
+        $sel = new List_Terms_rcl();
+        $catlist = $sel->get_select_list(get_public_allterms(),$cat_list,$cnt,$ctg);
+
+    }
+    if(!$catlist) return false;
+    
+    echo '<label>Категория:</label>'.$catlist;
+}
+
+function get_public_catlist(){       
+    global $formData;
+    
+    if(!isset($formData->taxonomy[$formData->post_type])) return false;
+
+    if($formData->post_type=='post'){ 
+        $cat_list = get_the_category($formData->post_id);
+    }else{
+        $post_cat = get_the_terms( $formData->post_id, $formData->taxonomy[$formData->post_type] );
+
+        $Child_Terms = new Get_Child_Terms_Rcl();
+        $cat_list = $Child_Terms->get_terms_post($post_cat);           
+    }
+    
+    return $cat_list;
+}
+
+function get_public_allterms(){
+    global $formData;
+    
+    if($formData->post_type&&!isset($formData->taxonomy[$formData->post_type])) return false;
+        
+    if($formData->post_type=='post'||!$formData->post_type){
+
+        $catargs = array(   
+            'orderby'   => 'name'  
+            ,'order'    => 'ASC'  
+            ,'hide_empty'   => 0   
+            ,'hierarchical' =>true
+        );
+
+        $allcats = get_categories( $catargs );
+
+    }else{
+
+        $term_args = array(  
+            'number'        => 0  
+            ,'offset'       => 0  
+            ,'orderby'      => 'id'  
+            ,'order'        => 'ASC'  
+            ,'hide_empty'   => false  
+            ,'fields'       => 'all'  
+            ,'slug'         => ''  
+            ,'hierarchical' => true  
+            ,'name__like'   => ''  
+            ,'pad_counts'   => false  
+            ,'get'          => ''  
+            ,'child_of'     => 0  
+            ,'parent'       => ''  
+        );  
+
+        $allcats = get_terms($formData->taxonomy[$formData->post_type], $term_args); 
+
+    }
+
+    return $allcats;
+}
+
+function the_public_editor(){
+    global $rcl_options,$editpost,$formData;
+        
+    $media_buttons = ($rcl_options['media_downloader_recall']==1)? $media_buttons = 1: 0;
+    $tinymce = ($formData->type_editor==1||$formData->type_editor==3)? $tinymce = 1: 0;
+    $quicktags = ($formData->type_editor==2||$formData->type_editor==3)? $quicktags = 1: 0;
+
+    $args = array( 'wpautop' => 1  
+        ,'media_buttons' => $media_buttons  
+        ,'textarea_name' => 'post_content'
+        ,'textarea_rows' => 20  
+        ,'tabindex' => null  
+        ,'editor_css' => ''  
+        ,'editor_class' => 'autosave'  
+        ,'teeny' => 0  
+        ,'dfw' => 0  
+        ,'tinymce' => $tinymce  
+        ,'quicktags' => $quicktags  
+    );
+
+    if($rcl_options['media_downloader_recall']!=1)
+        echo get_button_rcl('Добавить медиафайл','#',array('icon'=>'fa-folder-open','id'=>'get-media-rcl'));
+
+    $content = (isset($editpost->post_content))? $editpost->post_content: '';
+
+    wp_editor( $content, 'contentarea', $args );
+}
+
+function the_public_upload(){
+    global $formData;
+    new Rcl_Thumb_Form($formData->post_id,$formData->id_upload);
+}
+
+add_action('public_form','add_filter_public_form');
+function add_filter_public_form(){
+    global $formData;
+    $fields = '';
+    echo apply_filters('public_form_rcl',$fields,$formData);
+}
+
+function the_public_custom_fields(){
+    global $formData;
+    echo get_custom_fields_list_rcl($formData->post_id,$formData->post_type,$formData->form_id);
 }
---
+++ add-on/publicpost/templates/public-form.php
@@ -0,0 +1,28 @@
+<?php global $formFields,$editpost; ?>
+
+<?php if($formFields['title']): ?>
+    <label>Заголовок <span class="required">*</span>:</label>
+    <input type="text" maxlength="150" required value="<?php the_public_title(); ?>" name="post_title" id="post_title_input">
+<?php endif; ?>
+
+<?php if($formFields['termlist']): ?>   
+    <?php the_public_termlist(); ?>
+<?php endif; ?>
+
+<?php if($formFields['editor']): ?>
+    <label>Содержимое публикации</label>
+    <?php the_public_editor(); ?>
+<?php endif; ?>
+
+<?php if($formFields['upload']): ?>
+    <b>Нажмите на прикреленное изображение, чтобы добавить его в контент публикации</b>
+    <?php the_public_upload(); ?>
+<?php endif; ?>
+
+<?php do_action('public_form'); ?>
+
+<?php if($formFields['custom_fields']): ?> 
+    <?php the_public_custom_fields(); ?>
+<?php endif; ?>
+
+               
