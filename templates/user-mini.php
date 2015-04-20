8963ec94e49bd51b691af5f6111685621886537e
Андрей <@192.168.1.46>
2015-04-20 14:48:06 +0300

    указание размера аватара - 50 пикс.

--- templates/user-mini.php
+++ templates/user-mini.php
@@ -2,7 +2,7 @@
 <div class="user-single">
     <div class="thumb-user">
         <a title="<?php the_user_name(); ?>" href="<?php the_user_url(); ?>">
-            <?php the_user_avatar(70); ?>
+            <?php the_user_avatar(50); ?>
             <?php the_user_action(); ?>
         </a>
     </div>
