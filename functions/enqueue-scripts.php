7a6e9c712b3787c7f282bfe4ec64ff71f3b8d646
Андрей <@192.168.1.39>
2015-04-25 14:24:50 +0300

    локализация плагина и дополнений

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -1,11 +1,17 @@
 <?php
+
+add_action( 'plugins_loaded', 'rcl_load_plugin_textdomain' );
+function rcl_load_plugin_textdomain() {
+    load_theme_textdomain('rcl', RCL_PATH.'languages/');
+}
+
 add_action('wp','get_init_filters_actions_rcl');
 function get_init_filters_actions_rcl(){
     global $rcl_options,$user_ID,$user_LK;
 
     if (!is_admin()):
         add_action('wp_enqueue_scripts', 'output_style_scripts_recall');
-        add_filter('get_comment_author_url', 'add_link_author_in_page');					
+        add_filter('get_comment_author_url', 'add_link_author_in_page');
         add_action('wp_head','hidden_admin_panel');
 
         if(!$user_ID){
@@ -13,12 +19,12 @@ function get_init_filters_actions_rcl(){
             if($rcl_options['login_form_recall']==1) add_filter('wp_enqueue_scripts', 'script_page_form_recall');
             else if(!$rcl_options['login_form_recall']) add_filter('wp_enqueue_scripts', 'script_float_form_recall');
         }
-        
+
         if($user_LK) add_bxslider_scripts();
-        
+
     endif;
-    
-    add_action('wp_head','rcl_update_timeaction_user');       
+
+    add_action('wp_head','rcl_update_timeaction_user');
     add_action('before_delete_post', 'delete_attachments_with_post_rcl');
 
 }
@@ -43,13 +49,13 @@ function add_sortable_scripts(){
     wp_enqueue_script('jquery-ui-sortable');
 }
 
-function add_resizable_scripts(){   
+function add_resizable_scripts(){
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script('jquery-ui-resizable');
 }
 
 function add_datepicker_scripts(){
-    wp_enqueue_style( 'datepicker', RCL_URL.'js/datepicker/style.css' );   
+    wp_enqueue_style( 'datepicker', RCL_URL.'js/datepicker/style.css' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script('jquery-ui-core');
     wp_enqueue_script('jquery-ui-datepicker');
@@ -57,12 +63,12 @@ function add_datepicker_scripts(){
 }
 
 function add_bxslider_scripts(){
-    wp_enqueue_style( 'bx-slider', RCL_URL.'js/jquery.bxslider/jquery.bxslider.css' );   
+    wp_enqueue_style( 'bx-slider', RCL_URL.'js/jquery.bxslider/jquery.bxslider.css' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'bx-slider', RCL_URL.'js/jquery.bxslider/jquery.bxslider.min.js' );
     wp_enqueue_script( 'custom-bx-slider', RCL_URL.'js/slider.js', array('bx-slider'));
 }
-	
+
 function output_style_scripts_recall(){
 	global $rcl_options,$user_LK,$user_ID;
 	if(!isset($rcl_options['font_icons']))  $rcl_options['font_icons']=1;
@@ -70,17 +76,17 @@ function output_style_scripts_recall(){
 	if($user_ID==$user_LK) wp_enqueue_style( 'fileapi_jcrop', RCL_URL.'js/fileapi/jcrop/jquery.Jcrop.min.css' );
 
         if( wp_style_is( 'font-awesome' ) ) wp_deregister_style('font-awesome');
-        wp_enqueue_style( 'font-awesome', RCL_URL.'fonts/font-awesome.min.css', array(), '4.3.0' );
-        
+        wp_enqueue_style( 'font-awesome', RCL_URL.'css/fonts/font-awesome.min.css', array(), '4.3.0' );
+
 	if(isset($rcl_options['minify_css'])&&$rcl_options['minify_css']==1){
 		if($rcl_options['custom_scc_file_recall']!=''){
 			wp_enqueue_style( 'style_custom_rcl', $rcl_options['custom_scc_file_recall'] );
-		}else{		
+		}else{
 			wp_enqueue_style( 'style_recall', TEMP_URL.'css/minify.css' );
 		}
-	}else{	
+	}else{
             $css_ar = array('lk','recbar','regform','slider','users','style');
-            foreach($css_ar as $name){wp_enqueue_style( 'style_'.$name, RCL_URL.'css/'.$name.'.css' );}		
+            foreach($css_ar as $name){wp_enqueue_style( 'style_'.$name, RCL_URL.'css/'.$name.'.css' );}
 	}
 	if($rcl_options['color_theme']){
             $dirs   = array(RCL_PATH.'css/themes',TEMPLATEPATH.'/recall/themes');
@@ -89,30 +95,30 @@ function output_style_scripts_recall(){
                 wp_enqueue_style( 'theme_rcl', path_to_url_rcl($dir.'/'.$rcl_options['color_theme'].'.css') );
             }
         }
-	
+
 	wp_enqueue_script( 'jquery' );
-        
-	//if($user_ID) wp_enqueue_script( 'rangyinputs', RCL_URL.'js/rangyinputs.js' );
-        
+
+	if($user_ID) wp_enqueue_script( 'rangyinputs', RCL_URL.'js/rangyinputs.js' );
+
 	wp_enqueue_script( 'recall_recall', RCL_URL.'js/recall.js', array(), VER_RCL );
 	if(!file_exists(TEMP_PATH.'scripts/header-scripts.js')){
 		$rcl_addons = new rcl_addons();
 		$rcl_addons->get_update_scripts_file_rcl();
 	}
-	wp_enqueue_script( 'temp-scripts-recall', TEMP_URL.'scripts/header-scripts.js', array(), VER_RCL );	
+	wp_enqueue_script( 'temp-scripts-recall', TEMP_URL.'scripts/header-scripts.js', array(), VER_RCL );
 }
 
 function output_script_style_admin_recall(){
     wp_enqueue_style( 'admin_recall', RCL_URL.'css/admin.css' );
     wp_enqueue_script( 'jquery' );
-    wp_enqueue_script( 'primary_script_admin_recall', RCL_URL.'js/admin.js', array(), VER_RCL );	
+    wp_enqueue_script( 'primary_script_admin_recall', RCL_URL.'js/admin.js', array(), VER_RCL );
 }
 
 function fileapi_footer_scripts() {
     global $user_ID;
     if(!$user_ID) return false;
     if(file_exists(TEMP_PATH.'scripts/footer-scripts.js')){
-        wp_enqueue_script( 'jquery' );	
+        wp_enqueue_script( 'jquery' );
         wp_enqueue_script( 'FileAPI-min', RCL_URL.'js/fileapi/FileAPI/FileAPI.min.js', array(), VER_RCL, true );
         wp_enqueue_script( 'mousewheel-js', RCL_URL.'js/fileapi/FileAPI/FileAPI.exif.js', array(), VER_RCL, true );
         wp_enqueue_script( 'fileapi-pack-js', RCL_URL.'js/fileapi/jquery.fileapi.js', array(), VER_RCL, true );
--- load-rcl.php
+++ load-rcl.php
@@ -4,10 +4,10 @@ define('SHORTINIT',true);
 $path_parts = pathinfo(__FILE__);
 preg_match_all("/(?<=)[A-z0-9\-\/\.\_\s\ё]*(?=wp\-content)/i", $path_parts['dirname'], $string_value);
 
+global $wpdb,$user_ID,$rcl_options,$action,$active_plugins,$active_addons,$locale;
+
 require_once( $string_value[0][0].'/wp-load.php' );
 require_once( $string_value[0][0].'/wp-includes/l10n.php' );
-
-global $wpdb,$user_ID,$rcl_options,$action,$active_plugins,$active_addons,$locale;
 
 $rcl_options = get_option('primary-rcl-options');
 $active_plugins = serialize(get_option('active_plugins'));
@@ -22,4 +22,4 @@ require_once('functions/replacement.php');
 if(isset($_POST['action'])) $action = esc_sql($_POST['action']);
 if(isset($_POST['user_ID'])) $user_ID = esc_sql($_POST['user_ID']);
 
-load_theme_textdomain('rcl', RCL_PATH.'lang/');
+load_theme_textdomain('rcl', RCL_PATH.'languages/');
