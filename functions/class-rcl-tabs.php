6bd6cf023c26200530a91a13a610361bdde1b2f0
Андрей <@192.168.1.39>
2016-07-19 10:28:40 +0300

    15.0.6

--- functions/class-rcl-tabs.php
+++ functions/class-rcl-tabs.php
@@ -128,8 +128,8 @@ class Rcl_Tabs{
         
         switch($this->args['public']){
             case 0: if(!$user_ID||$user_ID!=$author_lk) return false; break;
-            //case -1: if(!$user_ID||$user_ID==$author_lk) return false; break;
-            //case -2: if($user_ID&&$user_ID==$author_lk) return false; break;
+            case -1: if(!$user_ID||$user_ID==$author_lk) return false; break;
+            case -2: if($user_ID&&$user_ID==$author_lk) return false; break;
         }
 
         $link = rcl_format_url(get_author_posts_url($author_lk),$this->id);
