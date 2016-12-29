c8bb7cbc124f1290883c9b9da5c6d95eaa312998
Андрей <@192.168.1.39>
2016-12-29 20:46:11 +0300

    15.6.1

--- wp-recall.php
+++ wp-recall.php
@@ -3,7 +3,7 @@
     Plugin Name: WP-Recall
     Plugin URI: http://codeseller.ru/?p=69
     Description: Фронт-енд профиль, система личных сообщений и рейтинг пользователей на сайте вордпресс.
-    Version: 15.6.0
+    Version: 15.6.1
     Author: Plechev Andrey
     Author URI: http://codeseller.ru/
     Text Domain: wp-recall
