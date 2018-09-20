6c9b6a84604aba317a7a9df0f04f33f1f72a2c33
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2018-09-20 16:27:41 +0400

    Каждое произвольное поле публикации имеет класс вида rcl-cf-type-["тип поля"] - для кастомизаций просили неоднократно Избавился от параграфа "p" - каждая тема его по своему стилизует. В списке публикаций (вкладка в ЛК) если у публикации нет заголовка - то не перейти в нее. Теперь есть троеточие "..." у записей без заголовка Поставил на место кнопку "удалить публикацию"

--- add-on/publicpost/templates/posts-list.php
+++ add-on/publicpost/templates/posts-list.php
@@ -1,42 +1,47 @@
+<?php
+/*  Шаблон базового дополнения PublicPost (Публикация) https://codeseller.ru/?p=7084
+    Если вам нужно внести изменения в данный шаблон - скопируйте его в папку /wp-content/wp-recall/templates/
+    - сделайте там в нём нужные вам изменения и он будет подключаться оттуда
+    Подробно работа с шаблонами описана тут: https://codeseller.ru/?p=11632
+*/
+?>
 <?php global $post,$posts,$ratings; ?>
 
 <table class="publics-table-rcl rcl-form">
+    <tr>
+        <th><?php _e('Date','wp-recall'); ?></th>
+        <th><?php _e('Title','wp-recall'); ?></th>
+        <th><?php _e('Status','wp-recall'); ?></th>
+    </tr>
 
-	<tr>
-            <th><?php _e('Date','wp-recall'); ?></th>
-            <th><?php _e('Title','wp-recall'); ?></th>
-            <th><?php _e('Status','wp-recall'); ?></th>
-	</tr>
+    <?php foreach($posts as $postdata){ ?>
+        <?php foreach($postdata as $post){ setup_postdata($post); ?>
+            <?php if($post->post_status=='pending') $status = '<span class="status-pending">'.__('to be approved','wp-recall').'</span>';
+            elseif($post->post_status=='trash') $status = '<span class="status-pending">'.__('deleted','wp-recall').'</span>';
+            elseif($post->post_status=='draft') $status = '<span class="status-draft">'.__('draft','wp-recall').'</span>';
+            else $status = '<span class="status-publish">'.__('published','wp-recall').'</span>'; ?>
 
-	<?php foreach($posts as $postdata){ ?>
+            <tr>
+                <td>
+                    <?php echo mysql2date('d.m.y', $post->post_date); ?>
+                </td>
 
-		<?php foreach($postdata as $post){ setup_postdata($post); ?>
+                <td>
+                    <?php if(empty($post->post_title)) $post->post_title = "<i class='rcli fa-ellipsis-h' aria-hidden='true'></i>"; ?>
+                    <?php echo ($post->post_status=='trash')? $post->post_title: '<a target="_blank" href="'.$post->guid.'">'.$post->post_title.'</a>'; ?>
 
-			<?php if($post->post_status=='pending') $status = '<span class="status-pending">'.__('to be approved','wp-recall').'</span>';
-			elseif($post->post_status=='trash') $status = '<span class="status-pending">'.__('deleted','wp-recall').'</span>';
-                        elseif($post->post_status=='draft') $status = '<span class="status-draft">'.__('draft','wp-recall').'</span>';
-			else $status = '<span class="status-publish">'.__('published','wp-recall').'</span>'; ?>
+                    <?php if(function_exists('rcl_format_rating')) {
+                        $rtng = (isset($ratings[$post->ID]))? $ratings[$post->ID]: 0;
+                        echo rcl_rating_block(array('value'=>$rtng));
+                    } ?>
+                    <?php $content = ''; echo apply_filters('content_postslist',$content); ?>
+                </td>
 
-			<tr>
+                <td>
+                    <?php echo $status ?>
+                </td>
+            </tr>
 
-				<td><?php echo mysql2date('d.m.y', $post->post_date); ?></td>
-
-				<td>
-
-				<?php echo ($post->post_status=='trash')? $post->post_title: '<a target="_blank" href="'.$post->guid.'">'.$post->post_title.'</a>'; ?>
-
-				<?php if(function_exists('rcl_format_rating')) {
-					$rtng = (isset($ratings[$post->ID]))? $ratings[$post->ID]: 0;
-					echo rcl_rating_block(array('value'=>$rtng));
-				} ?>
-				<?php $content = ''; echo apply_filters('content_postslist',$content); ?>
-
-				</td>
-				<td><?php echo $status ?></td>
-
-			</tr>
-		<?php } ?>
-
-	<?php } ?>
-
+        <?php } ?>
+    <?php } ?>
 </table>
